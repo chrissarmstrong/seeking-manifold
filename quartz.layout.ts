9d3fafbdafaaf0d6974e52014a885ca3c4a11bb7
Chris Armstrong <chrissarmstrong@aol.com>
2024-03-06 22:54:30 -0500

    Quartz sync: Mar 6, 2024, 10:54 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,8 +27,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-//    Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+    Component.DesktopOnly(Component.Explorer()),
+//    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
   ],
   right: [
 //    Component.Graph(),
@@ -45,8 +45,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-//    Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+    Component.DesktopOnly(Component.Explorer()),
+//    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
   ],
   right: [],
 }
