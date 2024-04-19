4a8f99ebfce5eecf3da594ae171bec2087d2fd37
Chris Armstrong <chrissarmstrong@aol.com>
2024-04-19 14:38:39 -0400

    modified:   quartz.layout.ts 	content/Home & Garden/index.md

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,9 +27,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+//    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "collapse"})),
 //    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
-  ],
+Component.Explorer({folderClickBehavior: "link", folderDefaultState: "open"}),
+],
   right: [
 //    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -45,8 +46,9 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+//    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "collapse"})),
 //    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+    Component.Explorer({folderClickBehavior: "link", folderDefaultState: "open"}),
   ],
   right: [],
 }
